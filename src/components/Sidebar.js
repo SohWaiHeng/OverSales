1587aeba841ef9b2106c8ff8833b8d87dd0f6c2d
neoys97 <neoys97@gmail.com>
2021-02-28 18:11:16 +0800

    jasper shuai

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,13 +1,13 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { SearchIcon } from "@chakra-ui/icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAlignJustify, faFilter, faMapMarkerAlt, faBox, faDollarSign, faTimes } from "@fortawesome/free-solid-svg-icons";
 import Color from "../common/Color";
 import styled from 'styled-components';
 import { Box, Button, Flex, Stack, Text, Drawer, DrawerBody, DrawerOverlay, 
-    DrawerContent, useDisclosure, FormControl, FormLabel, 
-    FormErrorMessage, Input, InputGroup, InputRightElement, Select } from "@chakra-ui/react";
+    DrawerContent, useDisclosure, FormControl, FormLabel, Input, InputGroup, InputRightElement, Select } from "@chakra-ui/react";
 import Stars from "../components/Stars";
+import CountryList from "../common/Country";
 
 const ButtonContainer = styled.div`
     :hover {
@@ -31,13 +31,31 @@ const Measurement = ({text}) => {
     )
 }
 
-const Sidebar = () => {
+const Sidebar = (props) => {
     const { isOpen, onOpen, onClose } = useDisclosure();
-    const [starNumber, changeStarNumber] = useState(0);
     const initialRef = useRef();
+    const [countries, setCountries] = useState([]);
+
     const starChanged = (n)=>{
-        changeStarNumber(n);
+        props.setRating(n);
     }
+
+    const resetFilter = () => {
+        props.setSearchName("");
+        props.setFromDestination("");
+        props.setToDestination("");
+        props.setWidth(0);
+        props.setHeight(0);
+        props.setLength(0);
+        props.setWeight(0);
+        props.setMinPrice(0);
+        props.setMaxPrice(0);
+        props.setRating(0);
+    }
+
+    useEffect(()=>{
+        setCountries(Object.keys(CountryList));
+    }, [])
 
     return (
         <Box>
@@ -61,7 +79,7 @@ const Sidebar = () => {
                             <FormControl>
                                 <Label fontSize = "20px">Search by profile</Label>
                                 <InputGroup color = "#3AAFA9">
-                                    <InputHolder />
+                                    <InputHolder onInput={(e)=>{props.setSearchName(e.target.value)}} value={props.searchName}/>
                                     <InputRightElement children = {<SearchIcon />} />
                                 </InputGroup>
 
@@ -76,19 +94,17 @@ const Sidebar = () => {
                                 </Flex>
 
                                 <Label mt = "5px">From:</Label>
-                                <Select color = "#3AAFA9" bgColor = "#FEFFFF" rounded = "20px" placeholder = "Select your origin">
-                                    <option>1</option>
-                                    <option>1</option>
-                                    <option>1</option>
-                                    <option>1</option>
+                                <Select color = "#3AAFA9" bgColor = "#FEFFFF" rounded = "20px" onInput={(e)=>{props.setFromDestination(e.target.value)}} placeholder = {props.fromDestination ? props.fromDestination : "Select your origin"}>
+                                    {countries.map((val)=>{
+                                        return(<option>{val}</option>)
+                                    })}
                                 </Select>
 
                                 <Label mt = "12px">To:</Label>
-                                <Select color = "#3AAFA9" bgColor = "#FEFFFF" rounded = "20px" placeholder = "Select your destination">
-                                    <option>1</option>
-                                    <option>1</option>
-                                    <option>1</option>
-                                    <option>1</option>
+                                <Select color = "#3AAFA9" bgColor = "#FEFFFF" rounded = "20px" onInput={(e)=>{props.setToDestination(e.target.value)}} placeholder = {props.toDestination ? props.toDestination : "Select your origin"}>
+                                    {countries.map((val)=>{
+                                        return(<option>{val}</option>)
+                                    })}
                                 </Select>
                                 
                                 <Flex mt = "30px" align = "center">
@@ -100,17 +116,17 @@ const Sidebar = () => {
                                     <Stack spacing = {3}>
                                         <Flex align = "center">
                                             <Label mr = "7px">Width:</Label>
-                                            <InputHolder width = "50%" ml = "5px" />
+                                            <InputHolder width = "50%" ml = "5px" onInput={(e)=>{props.setWidth(e.target.value ? Number(e.target.value) : 0)}} placeholder={props.width} type="number"/>
                                             <Measurement text = "cm"/>
                                         </Flex>
                                         <Flex align = "center">
                                             <Label mr = "7px">Height:</Label>
-                                            <InputHolder width = "50%"/>
+                                            <InputHolder width = "50%" onInput={(e)=>{props.setHeight(e.target.value ? Number(e.target.value) : 0)}} placeholder={props.height} type="number"/>
                                             <Measurement text = "cm"/>
                                         </Flex>
                                         <Flex align = "center">
                                             <Label mr = "7px">Length:</Label>
-                                            <InputHolder width = "50%"/>
+                                            <InputHolder width = "50%" onInput={(e)=>{props.setLength(e.target.value ? Number(e.target.value) : 0)}} placeholder={props.length} type="number"/>
                                             <Measurement text = "cm"/>
                                         </Flex>
                                     </Stack>
@@ -118,7 +134,7 @@ const Sidebar = () => {
                                         <Box>
                                             <Label>Weight:</Label>
                                             <Flex align = "center">
-                                                <InputHolder width = "65%" />
+                                                <InputHolder width = "65%" onInput={(e)=>{props.setWeight(e.target.value ? Number(e.target.value) : 0)}} placeholder={props.weight} type="number"/>
                                                 <Measurement text = "kg"/>
                                             </Flex>
                                         </Box>
@@ -133,7 +149,7 @@ const Sidebar = () => {
                                     <Box>
                                         <Label>Minimum:</Label>
                                         <Flex align = "center">
-                                            <InputHolder />
+                                            <InputHolder placeholder={props.minPrice} onInput={(e)=>{props.setMinPrice(e.target.value ? Number(e.target.value) : 0)}} type="number"/>
                                             <Measurement text = "HKD"/>
                                         </Flex>
                                     </Box>
@@ -143,7 +159,7 @@ const Sidebar = () => {
                                     <Box>
                                         <Label>Maximum:</Label>
                                         <Flex align = "center">
-                                            <InputHolder />
+                                            <InputHolder placeholder={props.maxPrice} onInput={(e)=>{props.setMaxPrice(e.target.value ? Number(e.target.value) : 0)}} type="number"/>
                                             <Measurement text = "HKD"/>
                                         </Flex>
                                     </Box>
@@ -151,14 +167,14 @@ const Sidebar = () => {
 
                                 <Box mt = "20px">
                                     <FormLabel>Minimum Rating:</FormLabel>
-                                    <Stars number={starNumber} onChange={starChanged} interactable/>
+                                    <Stars number={props.rating} onChange={starChanged} interactable/>
                                 </Box>
                                 
                             </FormControl>
 
                             <Stack isInline spacing = "20px" mt = "100px" mb = "50px">
                                 <Button pb = "2px" rounded = "20px" size = "lg">Apply</Button>
-                                <Button pb = "2px" rounded = "20px" size = "lg" bgColor = "#FF616A" color = "#FEFFFF">Reset</Button>
+                                <Button pb = "2px" rounded = "20px" size = "lg" bgColor = "#FF616A" color = "#FEFFFF" onClick={resetFilter}>Reset</Button>
                             </Stack>
                         </DrawerBody>
                      </DrawerContent>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,12 +7,43 @@ import SmallCard from "../components/SmallCard";
 
 const Home = () => {
     const [filteredList, setFilteredList] = useState([]);
+    const [searchName, setSearchName] = useState("");
+    const [fromDestination, setFromDestination] = useState("");
+    const [toDestination, setToDestination] = useState("");
+    const [width, setWidth] = useState(0);
+    const [height, setHeight] = useState(0);
+    const [length, setLength] = useState(0);
+    const [weight, setWeight] = useState(0);
+    const [minPrice, setMinPrice] = useState(0);
+    const [maxPrice, setMaxPrice] = useState(0);
+    const [rating, setRating] = useState(0);
 
     useEffect(()=>{
         setFilteredList(SellerList);
     }, []);
     return (
-        <Layout sideBar={<Sidebar />}>
+        <Layout sideBar={<Sidebar
+                            searchName={searchName}
+                            setSearchName={setSearchName}
+                            fromDestination={fromDestination}
+                            setFromDestination={setFromDestination}
+                            toDestination={toDestination}
+                            setToDestination={setToDestination}
+                            width={width}
+                            setWidth={setWidth}
+                            height={height}
+                            setHeight={setHeight}
+                            length={length}
+                            setLength={setLength}
+                            weight={weight}
+                            setWeight={setWeight}
+                            minPrice={minPrice}
+                            setMinPrice={setMinPrice}
+                            maxPrice={maxPrice}
+                            setMaxPrice={setMaxPrice}
+                            rating={rating}
+                            setRating={setRating}
+        />}>
             <SmallCard />
         </Layout>
     );
