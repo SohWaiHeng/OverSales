4cbee732c8e3a6dd6bc6b20129e3b3436ffa7d19
SohWaiHeng <u3570950@connect.hku.hk>
2021-02-28 16:15:37 +0800

    Added country and seller data

---
+++ src/common/Country.js
@@ -0,0 +1,17 @@
+const CountryList = { 
+    'Australia': 'https://rccl-h.assetsadobe.com/is/image/content/dam/royal/content/destinations/australia/australia-sydney-opera-house.jpg?$750x667$', 
+    'Brazil': 'https://www.pandotrip.com/wp-content/uploads/2016/02/Rio3-1-980x699.jpg', 
+    'China': 'https://thumbor.forbes.com/thumbor/fit-in/1200x0/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5df7fb014e2917000783339f%2F0x0.jpg', 
+    'Hong Kong': 'https://media.starcruises.com//Content/Images/090010c4-5823-4ddc-96ca-b57deb4edf29_hong_kong.jpg', 
+    'France': 'https://www.counterpointresearch.com/wp-content/uploads/2019/05/France-Smartphone-Market.jpg', 
+    'Malaysia': 'https://specials-images.forbesimg.com/imageserve/5e5de9e944b78400068bdc42/960x0.jpg?fit=scale', 
+    'Korea': 'https://e2k9ube.cloudimg.io/s/cdn/x/https://edienet.s3.amazonaws.com/news/images/full_41106.jpg?v=28/10/2020%2009:45:00', 
+    'Japan': 'https://www.state.gov/wp-content/uploads/2019/04/Japan-2107x1406.jpg', 
+    'Singapore': 'https://thumbor.forbes.com/thumbor/fit-in/1200x0/filters%3Aformat%28jpg%29/https%3A%2F%2Fblogs-images.forbes.com%2Falexcapri%2Ffiles%2F2018%2F09%2FSingapore-1200x800.jpg', 
+    'England': 'https://images.ctfassets.net/zggpk8714k6l/6oxd3DmwtGscI4CSgE28uW/8067dc37171a9d66e414baed193876b4/london-taste-of-england-hero.jpg', 
+    'USA': 'https://static5.depositphotos.com/1030296/395/i/950/depositphotos_3958211-stock-photo-new-york-cityscape-tourism-concept.jpg', 
+    'Mexico': 'https://post.healthline.com/wp-content/uploads/2020/09/1105-Does_Mexico_Have_Better_Healthcare_Than_the_United_States-1296x728-header-1200x628.jpg',
+    'Spain': 'https://www.studyandgoabroad.com/wp-content/uploads/2018/10/studyspain.jpg', 
+    'Italy': 'https://www.atlanticcouncil.org/wp-content/uploads/2020/09/Rome-coroavirus-large-1024x683.jpg'}
+
+export default CountryList;
---
+++ src/common/Seller.js
@@ -0,0 +1,71 @@
+
+const SellerList = [
+    {
+        'name': 'Jasper Tan',
+        'biography': 'First I hit the drum, then I bang your mum',
+        'rating': 4,
+        'fromCountry': 'Japan',
+        'toCountry': 'Hong Kong',
+        'dimension': '100 x 100 x 100',
+        'weight': 200,
+        'price': 180,
+        'additionalInformation': 'You are perfect. To think anything less is as pointless as a river thinking that it’s got too many curves or that it moves too slowly or that its rapids are too rapid. Says who? You’re on a journey with no defined beginning, middle or end. There are no wrong twists and turns. There is just being. And your job is to be as you as you can be. This is why you’re here. To shy away from who you truly are would leave the world you-less. You are the only you there is and ever will be. I repeat, you are the only you there is and ever will be. Do not deny the world its one and only chance to bask in your brilliance.'
+    },
+    {
+        'name': 'Lee Kin Kuan',
+        'biography': 'First I hit the drum, then I bang your mum',
+        'rating': 4,
+        'fromCountry': 'Hong Kong',
+        'toCountry': 'Earlnie\'s heart',
+        'dimension': '150 x 200 x 180',
+        'weight': 520,
+        'price': 666,
+        'additionalInformation': 'You are perfect. To think anything less is as pointless as a river thinking that it’s got too many curves or that it moves too slowly or that its rapids are too rapid. Says who? You’re on a journey with no defined beginning, middle or end. There are no wrong twists and turns. There is just being. And your job is to be as you as you can be. This is why you’re here. To shy away from who you truly are would leave the world you-less. You are the only you there is and ever will be. I repeat, you are the only you there is and ever will be. Do not deny the world its one and only chance to bask in your brilliance.'
+    },
+    {
+        'name': 'Neo Yi Siang',
+        'bioGraphy': 'First I hit the drum, then I bang your mum',
+        'rating': 4,
+        'fromCountry': 'Kedah',
+        'toCountry': 'Ri Min\'s heart',
+        'dimension': '200 x 110 x 180',
+        'weight': 250,
+        'price': 369,
+        'additionalInformation': 'You are perfect. To think anything less is as pointless as a river thinking that it’s got too many curves or that it moves too slowly or that its rapids are too rapid. Says who? You’re on a journey with no defined beginning, middle or end. There are no wrong twists and turns. There is just being. And your job is to be as you as you can be. This is why you’re here. To shy away from who you truly are would leave the world you-less. You are the only you there is and ever will be. I repeat, you are the only you there is and ever will be. Do not deny the world its one and only chance to bask in your brilliance.'
+    },
+    {
+        'name': 'Soh Wai Heng',
+        'bioGraphy': 'First I hit the drum, then I bang your mum',
+        'rating': 4,
+        'fromCountry': 'Malaysia',
+        'toCountry': 'Hong Kong',
+        'dimension': '80 x 50 x 90',
+        'weight': 110,
+        'price': 58,
+        'additionalInformation': 'You are perfect. To think anything less is as pointless as a river thinking that it’s got too many curves or that it moves too slowly or that its rapids are too rapid. Says who? You’re on a journey with no defined beginning, middle or end. There are no wrong twists and turns. There is just being. And your job is to be as you as you can be. This is why you’re here. To shy away from who you truly are would leave the world you-less. You are the only you there is and ever will be. I repeat, you are the only you there is and ever will be. Do not deny the world its one and only chance to bask in your brilliance.'
+    },
+    {
+        'name': 'John Doe',
+        'bioGraphy': 'First I hit the drum, then I bang your mum',
+        'rating': 4,
+        'fromCountry': 'England',
+        'toCountry': 'China',
+        'dimension': '160 x 140 x 180',
+        'weight': 350,
+        'price': 270,
+        'additionalInformation': 'You are perfect. To think anything less is as pointless as a river thinking that it’s got too many curves or that it moves too slowly or that its rapids are too rapid. Says who? You’re on a journey with no defined beginning, middle or end. There are no wrong twists and turns. There is just being. And your job is to be as you as you can be. This is why you’re here. To shy away from who you truly are would leave the world you-less. You are the only you there is and ever will be. I repeat, you are the only you there is and ever will be. Do not deny the world its one and only chance to bask in your brilliance.'
+    },
+    {
+        'name': 'Tan Shan Huang',
+        'bioGraphy': 'First I hit the drum, then I bang your mum',
+        'rating': 4,
+        'fromCountry': 'USA',
+        'toCountry': 'Indonesia',
+        'dimension': '120 x 130 x 140',
+        'weight': 250,
+        'price': 180,
+        'additionalInformation': 'You are perfect. To think anything less is as pointless as a river thinking that it’s got too many curves or that it moves too slowly or that its rapids are too rapid. Says who? You’re on a journey with no defined beginning, middle or end. There are no wrong twists and turns. There is just being. And your job is to be as you as you can be. This is why you’re here. To shy away from who you truly are would leave the world you-less. You are the only you there is and ever will be. I repeat, you are the only you there is and ever will be. Do not deny the world its one and only chance to bask in your brilliance.'
+    }
+]
+
+export default SellerList;
