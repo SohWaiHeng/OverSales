32f4400652dd7177cdb9c810b85a85dc865de15f
Jasper <shanhuang88846@gmail.com>
2021-02-28 17:18:50 +0800

    SmallCard draft

--- src/App.js
+++ src/App.js
@@ -2,15 +2,11 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
-import Page1 from "./pages/Page1";
-import Page2 from "./pages/Page2";
 
 const App = () => {
     return (
         <Router>
             <Switch>
-                <Route exact path = {"/page1"} component = {Page1} />
-                <Route exact path = {"/page2"} component = {Page2} />
                 <Route path = {"/"} component = {Home} />
             </Switch>
         </Router>
--- src/components/SmallCard.js
+++ src/components/SmallCard.js
@@ -1,53 +1,29 @@
 import React from "react";
-import Layout from "../components/Layout";
 import Color from "../common/Color";
-import styled from 'styled-components'
-import { Flex, SimpleGrid, GridItem } from "@chakra-ui/react";
-import { Box, Image, Badge, StarIcon, Text } from "@chakra-ui/react";
+import { Box, Image,Flex, Text } from "@chakra-ui/react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-regular-svg-icons';
-import { faMapMarkerAlt, faBox } from '@fortawesome/free-solid-svg-icons';
-import Stars from "../components/Stars";
+import { faMapMarkerAlt, faBox, faDollarSign } from '@fortawesome/free-solid-svg-icons';
+import Stars from "./Stars";
 
 const property = {
     imageUrl: "https://cdn.thecrazytourist.com/wp-content/uploads/2018/08/ccimage-shutterstock_571221049.jpg",
     imageAlt: "Kuala Lumpur, Malaysia"
 }
 
-const Cart = styled(Box)`
-    box-shadow: 0px 1px 5px ${Color.green4};
-    height: 100px;
-    padding-top: 50px;
-    padding-bottom: 50px;
-    
-`
 
-const Page1 = () => {
+const SmallCard = () => {
     return (
-        <Layout>
-            {/* <Grid
-                h="500px"
-                templateColumns="repeat(4, 1fr)"
-                gap={3}
-                columnGap="100px"
-                paddingTop="20px"
-                paddingLeft="200px"
-                paddingRight="50px"
-            >
-                <GridItem colSpan={2} imageUrl={property.imageUrl} />
-                <GridItem colSpan={2} bg="papayawhip" imageUrl={property.imageUrl} />
-            </Grid> */}
-            {/* <SimpleGrid columns={2}>
-                <Box maxW="sm" borderRadius="lg" d="flex" overflow="hidden" bgColor = "red">
-                    <Image src={property.imageUrl} alt={property.imageAlt} />
-                </Box>
-                <Box maxW="sm" borderRadius="lg" d="flex" alignItems="center" overflow="hidden">
-                    <Image src={property.imageUrl} alt={property.imageAlt} />
-                </Box>
-            </SimpleGrid> */}
+        <>
             <Flex justifyContent="space-around">
-                <Box height="650px" width="400px" borderRadius="lg">
-                    <Image height="200px" width="400px" borderRadius="lg" borderEndStartRadius="sm" borderEndEndRadius="sm" src={property.imageUrl} alt={property.imageAlt} />
+                <Box 
+                    height = "650px" 
+                    width = "400px" 
+                    rounded = "25px" 
+                    bgColor = {Color.green1}
+                    boxShadow = "2px 2px 5px"
+                    >
+                    <Image height="200px" width="400px" rounded = "25px" borderEndStartRadius="sm" borderEndEndRadius="sm" src={property.imageUrl} alt={property.imageAlt} />
                     <br></br>
                     <Box margin="0px 20px 0px 20px">
                         <Flex justifyContent="space-between">
@@ -55,25 +31,21 @@ const Page1 = () => {
                                 <FontAwesomeIcon icon={faUser} size="lg" />
                                 <Text ml="20px">Posted by</Text>
                             </Flex>
-                            <Stars></Stars>
+                            <Stars />
                         </Flex>
                         <Flex align="center">
                             <Text ml="37px" fontSize="20px" fontWeight="800">Jasper Tan</Text>
                         </Flex>
                     </Box>
                     <Box margin="20px 20px 0px 20px">
-                        {/* <Flex align="center" justifyContent="space-around">
-                            <FontAwesomeIcon icon={faMapMarkerAlt} size="lg" />
-                            <Text ml="20px">Posted by</Text>
-                        </Flex> */}
                         <Flex align="center" >
                             <FontAwesomeIcon icon={faMapMarkerAlt} size="lg" />
                             <Text ml="20px">From</Text>
-                            <Text ml="40px" fontSize="20px" fontWeight="800">Japan</Text>
+                            <Text ml="40px" fontSize="20px" fontWeight="700">Japan</Text>
                         </Flex>
                         <Flex align="center" >
                             <Text ml="37px">To</Text>
-                            <Text ml="60px" fontSize="20px" fontWeight="800">Hong Kong</Text>
+                            <Text ml="60px" fontSize="20px" fontWeight="700">Hong Kong</Text>
                         </Flex>
                     </Box>
                     <Box margin="20px 20px 0px 20px">
@@ -81,20 +53,25 @@ const Page1 = () => {
                             <FontAwesomeIcon icon={faBox} size="lg" />
                             <Text ml="17px">Dimension</Text>
                         </Flex>
-                        <Text ml="38px" fontSize="20px" fontWeight="800" mb="25px">200 cm x 200 cm x 300 cm</Text>
+                        <Text ml="38px" fontSize="20px" fontWeight="700" mb="25px">200 cm x 200 cm x 300 cm</Text>
                         <Text ml="37px" mb="7px">Weight</Text>
-                        <Text ml="38px" fontSize="20px" fontWeight="800" mb="18px">25 kg</Text>
+                        <Text ml="38px" fontSize="20px" fontWeight="700" mb="18px">25 kg</Text>
                     </Box>
-                    <Box>
+                    <Box margin = "20px 20px 0px 20px">
+                        <Flex align = "center">
+                            <FontAwesomeIcon icon = {faDollarSign} size = "lg" />
+                            <Text ml = "28px" fontSize = "20px" fontWeight = "800">87 HKD</Text>
+                        </Flex>
                         
                     </Box>
 
                 </Box>
-                <Box>
+                {/* <Box>
                     <Image height="200px" width="400px" borderRadius="lg" src={property.imageUrl} alt={property.imageAlt} />
-                </Box>
+                </Box> */}
             </Flex>
-            <Flex justifyContent="space-around">
+
+            {/* <Flex justifyContent="space-around">
                 <Box height="650px" width="400px" borderRadius="lg">
                     <Image height="200px" width="400px" borderRadius="lg" src={property.imageUrl} alt={property.imageAlt} />
                 </Box>
@@ -109,9 +86,9 @@ const Page1 = () => {
                 <Box>
                     <Image height="200px" width="400px" borderRadius="lg" src={property.imageUrl} alt={property.imageAlt} />
                 </Box>
-            </Flex>
-        </Layout>
+            </Flex> */}
+        </>
     )
 }
 
-export default Page1;
+export default SmallCard;
