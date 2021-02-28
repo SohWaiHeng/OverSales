3597917dffc4482140c61ef5c07c44cb293062b0
neoys97 <neoys97@gmail.com>
2021-02-28 13:46:12 +0800

    more handsome jasper

--- src/components/BigCard.js
+++ src/components/BigCard.js
@@ -3,15 +3,35 @@ import Color from "../common/Color";
 import styled from 'styled-components';
 
 const Container = styled.div`
-    width: 700px
+    width: 700px;
     min-height: 100px;
-    background: ${Color.green4};
+    background: ${Color.green1};
+    border-radius: 30px;
+    overflow-y: hidden;
+`
+
+const ImgContainer = styled.div`
+    height: 100px;
+    & img {
+        width: 100%;
+        object-fit: cover;
+        height: 100%;
+    }
+`
+
+const ContentContainer = styled.div`
+    
 `
 
 const BigCard = (props) => {
     return(
         <Container>
-
+            <ImgContainer>
+                <img src="https://www.globaltimes.cn/Portals/0/attachment/2020/2020-07-28/9f4a7501-dfb5-4bfd-8e2e-c66f32e47a91.jpeg"></img>
+            </ImgContainer>
+            <ContentContainer>
+                
+            </ContentContainer>
         </Container>
     )
 }
