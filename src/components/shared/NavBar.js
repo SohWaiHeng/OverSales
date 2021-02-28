59a197cab4747290cf14f4e6dd255b6537fc55c5
Jasper <shanhuang88846@gmail.com>
2021-02-28 09:18:21 +0800

    Navbar draft

--- src/components/shared/NavBar.js
+++ src/components/shared/NavBar.js
@@ -1,39 +1,52 @@
 import React from "react";
 import { Link as RouterLink } from "react-router-dom";
-import { Stack, Flex } from "@chakra-ui/react";
+import { Stack, Flex, Text, Image, Box } from "@chakra-ui/react";
 
-const pages = [
-    {
-        link: "/page1",
-        text: "Page 1"
-    },
-    {
-        link: "/page2",
-        text: "Page 2"
-    }
-]
+// const pages = [
+//     {
+//         link: "/page1",
+//         text: "Buy"
+//     },
+//     {
+//         link: "/page2",
+//         text: "Contact Us"
+//     }
+// ]
 
 const NavBar = () => {
     return (
         <Flex
-            justifyContent = "space-between"
-            alignItems = "center"
             px = {[2, 4, 8]}
             py = {3}>
             <RouterLink to = "/">
-                Home
+                <Flex fontSize = "64px">
+                    <Image />
+                    <Text color = "#2B7A78">Over</Text>
+                    <Text color = "#17252A">sales</Text>
+                </Flex>
             </RouterLink>
-            <Stack isInline spacing = {4}>
-                {
-                    pages.map((props, index) => {
-                        return (
-                            <RouterLink to = {props.link}>
-                                {props.text}
-                            </RouterLink>
-                        )
-                    })
-                }
-            </Stack>
+            <Flex width = "100%" justifyContent = "space-around">
+                <Stack isInline spacing = {4} fontSize = "32px">
+                    {/* {
+                        pages.map((props, index) => {
+                            return (
+                                <RouterLink to = {props.link}>
+                                    {props.text}
+                                </RouterLink>
+                            )
+                        })
+                    } */}
+                    <Box 
+                        as = "button" 
+                        __css = {{borderBottom: "5px solid red"}}>
+                        Buy
+                    </Box>
+                    <Box>
+                        Contact Us
+                    </Box>
+                </Stack>
+                
+            </Flex>
         </Flex>
     );
 }
